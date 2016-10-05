d615bc274bcafc92e5337845a462afbb15ef2858
chambodn <chambodn@skysoft-atm.com>
2016-10-05 16:24:00 +0200

    Handle timeout and address race condition

--- bin/start-node.sh
+++ bin/start-node.sh
@@ -8,12 +8,12 @@ if [ -f $HAZELCAST_CONFIG_FILE ]; then
 	echo $HAZELCAST_CONFIG_FILE exists
 else
 	echo $HAZELCAST_CONFIG_FILE does not exist. Wait for it.
-	inotifywait -m /opt/hazelcast -e create |
-		while read path action file; do
-			echo "The file '$file' appeared in directory '$path' via '$action'"
-			if [ $file == $HAZELCAST_FILE ]; then
-				break
-			fi
-		done
-		java -server -Dhazelcast.config=/opt/hazelcast/hazelcast.xml com.hazelcast.core.server.StartServer
+	while [ ! -f "$HAZELCAST_CONFIG_FILE" ]
+	do
+		# Timeout necessary to prevent a TOCTTOU race condition. Otherwise, inotifywait could hang 
+		# indefinitely if a file is created just before it starts listening for events.
+		inotifywait -qqt 60 -e create -e moved_to "$(dirname $HAZELCAST_CONFIG_FILE)"
+	done
 fi
+
+java -server -Dhazelcast.config=/opt/hazelcast/hazelcast.xml com.hazelcast.core.server.StartServer
